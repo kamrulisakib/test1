32efac46dc38a42d95f795476e7eca8590593911
kamrulisakib <mr.kamrul.sakib@gmail.com>
2023-02-07 10:03:17 +0600

    added a landing page

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html", name="John Doe", description="I am a software engineer.")
+
+if __name__ == "__main__":
+    app.run()
